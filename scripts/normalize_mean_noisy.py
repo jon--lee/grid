3ef9693ac77a5ca38ca92e2da85dced9d19464bd
Jonathan Lee <123abcjonathanlee@gmail.com>
2017-05-12 14:12:13 -0700

    before switching to hashmaps

--- scripts/normalize_mean_noisy.py
+++ scripts/normalize_mean_noisy.py
@@ -3,10 +3,10 @@ import IPython
 import matplotlib.pyplot as plt
 import os
 
-base_dir = 'comparisons/revisited/feb23/0'
+base_dir = 'comparisons/revisited/may3/4'
 # base_dir = 'comparisons/revisited/feb19_sweep_no_flag/3/'
 ref_root = 'comparisons/random2d_deter_sparse_linear'
-substr = '_20ld_-1d_70m_0.0pb_data'
+substr = '_10ld_-1d_70m_0.0pb_data'
 ref_substr = '_1ld_-1d_70m_data'
 
 
@@ -41,7 +41,8 @@ def plot(classic_il_data, dagger_data, label='Reward', filename='tmp.eps'):
     se_dagger = np.std(dagger_data, axis=0) / np.sqrt(dagger_data.shape[0])
 
     # save the normalized data for comparisons with others if needed
-    np.save('compilations2/potpourri20/supervise_20ld.npy', classic_il_data)
+    np.save('compilations_may3/svm/supervise_.4eps.npy', classic_il_data)
+    #np.save('compilations2/potpourri20/supervise_20ld.npy', classic_il_data)
     #np.save('compilations2/potpourri20/dagger_20ld.npy', dagger_data)
     
     x1 = range(len(mean_classic_il))
